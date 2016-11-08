bb240955274b7a32e2fc7b821c1eb1df724419a0
Eddie Song <eddie.y.song@gmail.com>
2016-11-07 23:31:02 -0500

    Added a door prefab that can be triggered, and adjusted walls to work with the door

---
+++ Assets/_Scripts/DoorControllerScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorControllerScript : MonoBehaviour {
+	
+	private AudioClip doorOpenSound, doorCloseSound;
+	private Animator doorAnimator;
+	private bool doorOpen = false;
+
+	// Use this for initialization
+	void Start () {
+		doorAnimator = GetComponentInParent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Called when the player activates the door
+	void Activate () {
+		
+		if (doorOpen) {
+			CloseDoor ();
+			doorOpen = false;
+		} else {
+			OpenDoor ();
+			doorOpen = true;
+			Debug.Log ("activate");
+		}
+	}
+
+	void OpenDoor() {
+		doorAnimator.SetTrigger ("Open");
+	}
+
+	void CloseDoor() {
+		doorAnimator.SetTrigger ("Close");
+	}
+}
---
+++ Assets/_Scripts/PlayerControllerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+//	public GameObject activateEffect;
+
+	private RaycastHit activate;
+	private float targetDistance;
+	[SerializeField] float allowedRange = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), out activate)) {
+				targetDistance = activate.distance;
+				if (targetDistance < allowedRange) {
+					Debug.Log (activate.transform.gameObject.tag);
+					activate.transform.gameObject.SendMessage ("Activate", null, SendMessageOptions.DontRequireReceiver);
+//					Instantiate (activateEffect, activate.point, Quaternion.identity);
+				}
+			}
+		}
+	}
+}
