b8afe6800c8bb1ac97453fd415aba8e0193c4b5e
Eddie Song <eddie.y.song@gmail.com>
2016-11-09 00:42:18 -0500

    Added flickering lights to enhance the atmosphere

---
+++ Assets/_Scripts/LightFlicker.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+	[SerializeField] private float flickerFrequency = 10f;
+	[SerializeField] private int numFlickers = 5;
+	[SerializeField] private float dimTime = 0.05f;
+	[SerializeField] private float brightTime = 0.2f;
+	private bool flickTrigger = true;
+	private float flickTimer;
+	private Light light;
+	private float initialIntensity;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+		initialIntensity = light.intensity;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > flickTimer + flickerFrequency) {
+			StartCoroutine ("Flicker");
+			flickTimer = Time.time;
+		}
+	}
+
+	IEnumerator Flicker () {
+		Debug.Log ("flicker");
+		if (flickTrigger) {
+			for (int i = 0; i < numFlickers; i++) {
+				Debug.Log ("for loop");
+				light.intensity = initialIntensity * Random.Range(0.1f, 0.5f);
+				yield return new WaitForSeconds(dimTime * Random.Range(0.1f, 2f));
+				light.intensity = initialIntensity;
+				yield return new WaitForSeconds(brightTime* Random.Range(0.1f, 2f));
+			}
+//			flickTrigger = false;
+			StopCoroutine ("Flicker");
+		}
+	}
+}
